286da355d9e942f6854dc8b8ea75f4986e5800ea
JustCode09 <Professionalacc011@gmail.com>
2026-05-09 09:47:34 +0545

    Add Topic 6: Classes and Objects - Student System

--- obj/Debug/net8.0/LearningCSharpNotes.AssemblyInfo.cs
+++ obj/Debug/net8.0/LearningCSharpNotes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningCSharpNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d707fd4481590b91247b201c8a21159e8f3daaae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47619ea7b9002f9849097e483d4dfd6918af7636")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningCSharpNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningCSharpNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
